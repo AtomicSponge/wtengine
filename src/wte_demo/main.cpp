4f710ef4ec388509399821a0ed37515b44e2f4fb
wtfsystems <mevans+git@wtfsystems.net>
2020-01-24 17:48:15 -0500

    Renamed game_loop to my_game

--- src/wte_demo/main.cpp
+++ src/wte_demo/main.cpp
@@ -20,14 +20,14 @@ int main(int argc, char **argv) {
     wte::wte_config::screen_height = 1024;
 
     //  Initialize game object
-    game_loop main_game_loop;
-    main_game_loop.wte_init();
+    game_loop my_game;
+    my_game.wte_init();
 
     //  Run the game loop
-    main_game_loop.do_game();
+    my_game.do_game();
 
     //  Clean up
-    main_game_loop.wte_unload();
+    my_game.wte_unload();
 
     return 0; //  Exit program
 }

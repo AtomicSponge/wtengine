d0501d5a9c5049d73d1e9bd9047a28ba0da9a7f5
wtfsystems <mevans+git@wtfsystems.net>
2020-02-25 01:05:51 -0500

    Updated message class

--- src/wte/message.hpp
+++ src/wte/message.hpp
@@ -31,16 +31,44 @@ class message {
 
         //!  Create a non-timed message
         inline message(std::string s, std::string c, std::string a) :
-                       timer(-1), sys(s), to(""), from(""), cmd(c), args(a) {};
+                       timer(-1), sys(s), to(""), from(""), cmd(c), args(a) {
+                            std::stringstream arg_stream(args);
+                            std::string segment;
+
+                            while(std::getline(arg_stream, segment, ';')) {
+                                arglist.push_back(segment);
+                            }
+                       };
         //!  Create a timed message
         inline message(int64_t e, std::string s, std::string c, std::string a) :
-                       timer(e), sys(s), to(""), from(""), cmd(c), args(a) {};
+                       timer(e), sys(s), to(""), from(""), cmd(c), args(a) {
+                            std::stringstream arg_stream(args);
+                            std::string segment;
+
+                            while(std::getline(arg_stream, segment, ';')) {
+                                arglist.push_back(segment);
+                            }
+                       };
         //!  Create a non-timed message with a to & from
         inline message(std::string s, std::string t, std::string f, std::string c, std::string a) :
-                       timer(-1), sys(s), to(t), from(f), cmd(c), args(a) {};
+                       timer(-1), sys(s), to(t), from(f), cmd(c), args(a) {
+                            std::stringstream arg_stream(args);
+                            std::string segment;
+
+                            while(std::getline(arg_stream, segment, ';')) {
+                                arglist.push_back(segment);
+                            }
+                       };
         //!  Create a timed message with a to & from
         inline message(int64_t e, std::string s, std::string t, std::string f, std::string c, std::string a) :
-                       timer(e), sys(s), to(t), from(f), cmd(c), args(a) {};
+                       timer(e), sys(s), to(t), from(f), cmd(c), args(a) {
+                            std::stringstream arg_stream(args);
+                            std::string segment;
+
+                            while(std::getline(arg_stream, segment, ';')) {
+                                arglist.push_back(segment);
+                            }
+                       };
 
         //!  Return the value of timer
         inline const int64_t get_timer(void) const { return timer; };
@@ -55,17 +83,23 @@ class message {
         //!  Return the value of args
         inline const std::string get_args(void) const { return args; };
 
+        //!  Split the args up into a vector of strings
+        //!  Returns a vector of the arguments, delimited by ;
+        inline const msg_arg_list get_split_args(void) const { return arglist; }
+
+        //!  Get a single argument
+        //!  Returns a string by index from the argument list, delimited by ;
+        inline const std::string get_split_args(const std::size_t pos) const {
+            if(pos > arglist.size()-1 || pos < 0) return "";  //  Out of range, return empty string
+            return arglist[pos];
+        }
+
         //!  Check if the event is synced to the timer
         //!  Returns false if the timer value is -1
         inline const bool is_timed_event(void) const {
             if(timer == -1) return false;
             else return true;
         }
-
-        //!  Split the args up into a vector of strings
-        const msg_arg_list get_split_args(void) const;
-        //!  Get a single argument
-        const std::string get_split_args(const std::size_t) const;
 
         //!  Used to sort by timer value
         bool operator<(const message& a) const {
@@ -79,40 +113,8 @@ class message {
         std::string from;   //  Message from entity field
         std::string cmd;    //  Message command
         std::string args;   //  Message arguments
+        msg_arg_list arglist;
 };
-
-/*!
-  Returns a vector of the arguments, delimited by ;
-*/
-inline const msg_arg_list message::get_split_args(void) const {
-    std::stringstream arg_stream(args);
-    std::string segment;
-    std::vector<std::string> arglist;
-
-    while(std::getline(arg_stream, segment, ';')) {
-        arglist.push_back(segment);
-    }
-
-    return arglist;
-}
-
-/*!
-  Returns a string by index from the argument list, delimited by ;
-*/
-inline const std::string message::get_split_args(const std::size_t pos) const {
-    std::stringstream arg_stream(args);
-    std::string segment;
-    std::vector<std::string> arglist;
-
-    while(std::getline(arg_stream, segment, ';')) {
-        arglist.push_back(segment);
-    }
-
-    //  Out of range, return empty string.
-    if(pos > arglist.size()-1 || pos < 0) return "";
-
-    return arglist[pos];
-}
 
 } //  namespace wte
 

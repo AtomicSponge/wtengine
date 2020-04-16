6997afe1c90ba7984c04dbae56dcabc57ac543e9
wtfsystems <mevans+git@wtfsystems.net>
2020-04-16 11:26:33 -0400

    sync

--- src/wte_demo/include/wte/mnu/menu.hpp
+++ src/wte_demo/include/wte/mnu/menu.hpp
@@ -33,62 +33,86 @@ typedef std::vector<menu_item_sptr>::const_iterator menu_item_citerator;
 class menu {
     public:
         /*!
-         *
+         * Description
+         * \param void
+         * \return void
          */
         inline menu(const std::string i, const std::string t) : id(i), title(t) { items.clear(); };
         
         /*!
-         *
+         * Description
+         * \param void
+         * \return void
          */
         inline ~menu() { items.clear(); };
 
         /*!
-         *
+         * Description
+         * \param void
+         * \return void
          */
         inline const std::string get_id(void) const { return id; };
         
         /*!
-         *
+         * Description
+         * \param void
+         * \return void
          */
         inline const std::string get_title(void) const { return title; };
         
         /*!
-         *
+         * Description
+         * \param void
+         * \return void
          */
         //inline menu_items get_items(void) const { return items; };
 
         /*!
-         *
+         * Description
+         * \param void
+         * \return void
          */
         inline std::size_t num_items(void) const { return items.size(); };
 
         /*!
-         *
+         * Description
+         * \param void
+         * \return void
          */
         inline menu_item_citerator items_begin() const { return items.begin(); };
         
         /*!
-         *
+         * Description
+         * \param void
+         * \return void
          */
         inline menu_item_citerator items_end() const { return items.end(); };
 
         /*!
-         *
+         * Description
+         * \param void
+         * \return void
          */
         inline menu_item_citerator items_cbegin() const { return items.cbegin(); };
         
         /*!
-         *
+         * Description
+         * \param void
+         * \return void
          */
         inline menu_item_citerator items_cend() const { return items.cend(); };
 
         /*!
-         *
+         * Description
+         * \param void
+         * \return void
          */
         inline void set_title(const std::string t) { title = t; };
 
         /*!
          * Add a menu item to an existing menu
+         * \param void
+         * \return void
          */
         inline const bool add_item(const menu_item_sptr item) {
             items.push_back(item);

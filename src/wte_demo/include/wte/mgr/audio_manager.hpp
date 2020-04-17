408d9c4d0dc24793938aa1e1abb0358849d878e7
wtfsystems <mevans+git@wtfsystems.net>
2020-04-17 00:55:52 -0400

    sync

--- src/wte_demo/include/wte/mgr/audio_manager.hpp
+++ src/wte_demo/include/wte/mgr/audio_manager.hpp
@@ -362,23 +362,11 @@ inline void audio_manager::run(void) {
                     pos = std::stoi(audio_messages.front().get_arg(0));
                     float vol = std::atof(audio_messages.front().get_arg(1).c_str());
                     if(vol >= 0 && vol <= 1.0) {
-                        switch(pos) {
-                            case 0:
-                                al_set_mixer_gain(mixer_main, vol);
-                                break;
-                            case 1:
-                                al_set_mixer_gain(mixer_1, vol);
-                                break;
-                            case 2:
-                                al_set_mixer_gain(mixer_2, vol);
-                                break;
-                            case 3:
-                                al_set_mixer_gain(mixer_3, vol);
-                                break;
-                            case 4:
-                                al_set_mixer_gain(mixer_4, vol);
-                                break;
-                        }
+                        if(pos == 0) al_set_mixer_gain(mixer_main, vol);
+                        if(pos == 1) al_set_mixer_gain(mixer_1, vol);
+                        if(pos == 2) al_set_mixer_gain(mixer_2, vol);
+                        if(pos == 3) al_set_mixer_gain(mixer_3, vol);
+                        if(pos == 4) al_set_mixer_gain(mixer_4, vol);
                     }
                     break;
 

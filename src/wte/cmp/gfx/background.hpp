374b990a1b2eb6c08cf6e48cf3fabfddc4832f0e
wtfsystems <mevans+git@wtfsystems.net>
2020-06-07 23:01:20 -0400

    sync

--- src/wte/cmp/gfx/background.hpp
+++ src/wte/cmp/gfx/background.hpp
@@ -12,6 +12,8 @@
 #ifndef WTE_CMP_BACKGROUND_HPP
 #define WTE_CMP_BACKGROUND_HPP
 
+#include <string>
+
 #include <allegro5/allegro.h>
 
 #include "../../engine_cfg_map.hpp"
@@ -30,17 +32,57 @@ namespace cmp
 class background final : public animator {
     public:
         /*!
-         * Static background.
-         * \param void
+         * Static background, solid color.
+         * \param w Background width.
+         * \param h Background height.
+         * \param c Allegro color.
+         * \param l Background layer.
          * \return void
          */
-        inline background(const int w, const int h, const std::size_t l) : animator(l, {}) {
+        inline background(const int w,
+                          const int h,
+                          ALLEGRO_COLOR c,
+                          const std::size_t l) :
+        animator(l, {}) {
             background_bitmap = al_create_bitmap(w, h);
+            al_set_target_bitmap(background_bitmap);
+            al_clear_to_color(c);
+        };
+
+        /*!
+         * Static background, load image from file.
+         * Image is scaled to fit the background bitmap.
+         * \param w Background width.
+         * \param h Background height.
+         * \param fname File name to load.
+         * \param l Background layer.
+         * \return void
+         */
+        inline background(const int w,
+                          const int h,
+                          std::string fname,
+                          const std::size_t l) :
+        animator(l, {}) {
+            background_bitmap = al_create_bitmap(w, h);
+
+            ALLEGRO_BITMAP* temp_bmp;
+            ALLEGRO_FILE* file;
+            file = al_fopen(fname.c_str(), "rb");
+            temp_bmp = al_load_bitmap_f(file, NULL);
+            al_fclose(file);
+
+            al_set_target_bitmap(background_bitmap);
+            al_draw_scaled_bitmap(temp_bmp, 0, 0, al_get_bitmap_width(temp_bmp), al_get_bitmap_height(temp_bmp),
+                                  0, 0, al_get_bitmap_width(background_bitmap), al_get_bitmap_height(background_bitmap), 0);
+            al_destroy_bitmap(temp_bmp);
         };
 
         /*!
          * Background with custom animation.
-         * \param void
+         * \param w Background width.
+         * \param h Background height.
+         * \param l Background layer.
+         * \param func Animcation function.
          * \return void
          */
         inline background(const int w,
@@ -60,7 +102,7 @@ class background final : public animator {
             al_destroy_bitmap(background_bitmap);
         };
 
-        ALLEGRO_BITMAP *background_bitmap;
+        ALLEGRO_BITMAP* background_bitmap;
 };
 
 } //  namespace cmp

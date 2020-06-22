b7eb4f55a24eb097d8788102813161ba43e6279c
wtfsystems <mevans+git@wtfsystems.net>
2020-06-22 10:17:18 -0400

    sync

--- src/wte_demo/main.cpp
+++ src/wte_demo/main.cpp
@@ -1,12 +1,12 @@
 /*
-  WTEngine Demo
-  By:  Matthew Evans
-  File:  main.cpp
-
-  See LICENSE.txt for copyright information
-
-  Main file for WTEngine demo
-*/
+ * WTEngine Demo
+ * By:  Matthew Evans
+ * File:  main.cpp
+ *
+ * See LICENSE.txt for copyright information
+ *
+ * Main file for WTEngine demo
+ */
 
 #include <wte/wte.hpp>
 
@@ -27,8 +27,9 @@ int main(int argc, char **argv) {
     //mgr::render_manager::set_arena_size(1920, 1080);
 
     //  Register default engine settings.
-    engine_cfg::reg("draw_fps=0");
     engine_cfg::reg("display_mode=windowed");
+    engine_cfg::reg("scale_factor=1");
+    engine_cfg::reg("draw_fps=0");
 
     //  Load settings.
     engine_cfg_map::load();

999cb980ca9b72eccd26798b83445650112d3720
wtfsystems <mevans+git@wtfsystems.net>
2020-06-26 22:20:07 -0400

    sync

--- src/wte/sys/input.hpp
+++ src/wte/sys/input.hpp
@@ -48,12 +48,12 @@ class input final : public system {
             component_container<cmp::input_handler> input_components =
                 world.set_components<cmp::input_handler>();
 
-            for(auto & it : input_components) {  //  Loop all input components
-                for(auto & i_it : it.second->input_map) {  //  Loop the input map for each component
+            for(auto & c_it : input_components) {  //  Loop all input components
+                for(auto & i_it : c_it.second->input_map) {  //  Loop the input map for each component
                     if(input_flags::is_set(i_it.first))  //  Button is down
-                        i_it.second.first(it.first, world, messages, current_time);
+                        i_it.second.first(c_it.first, world, messages, current_time);
                     else  //  Button is up
-                        i_it.second.second(it.first, world, messages, current_time);
+                        i_it.second.second(c_it.first, world, messages, current_time);
                 }  //  End input map loop
             }  //  End input component loop
         };

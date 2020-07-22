057ec0a558c25165b40f637836f797978e290111
wtfsystems <mevans+git@wtfsystems.net>
2020-07-22 19:58:21 -0400

    sync

--- src/wte_demo/wte_demo.cpp
+++ src/wte_demo/wte_demo.cpp
@@ -601,7 +601,7 @@ void wte_demo::new_game(void) {
                     //  Health check.  If asteroid's HP is <= 0, reward player with points and delete the entity.
                     if(wte_get_component(ast_id, health)->hp <= 0) {
                         messages.add_message(message("spawner", "delete", world.get_name(ast_id)));
-                        messages.add_message(message("audio", "play_sample", "megumin;once"));
+                        messages.add_message(message("audio", "play_sample", "megumin;once;;;1.8"));
                         game_cfg::add<int>("score", (10 * wte_get_component(ast_id, size)->the_size));
 
                         //  If the asteroid was size >= 4, split into two.

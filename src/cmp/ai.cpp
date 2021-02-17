bbe082b1373dc84204aff254901690ffb6411036
Matthew Evans <mevans+git@wtfsystems.net>
2021-02-17 08:23:32 -0500

    sync

--- src/cmp/ai.cpp
+++ src/cmp/ai.cpp
@@ -12,9 +12,9 @@
 
 #include <functional>
 
-#include "component.hpp"
-#include "../mgr/entity_manager.hpp"
-#include "../mgr/message_manager.hpp"
+#include "wtengine/component.hpp"
+#include "wtengine/mgr/entity_manager.hpp"
+#include "wtengine/mgr/message_manager.hpp"
 
 namespace wte
 {

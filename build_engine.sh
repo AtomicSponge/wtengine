326b878b05b33cd1d25b11fe42b120db67b39f9e
Matthew Evans <mevans+git@wtfsystems.net>
2021-02-18 09:27:10 -0500

    sync

--- build_engine.sh
+++ build_engine.sh
@@ -9,24 +9,26 @@ SOURCE_DIR="/src"
 ##################################################
 build_items()
 {
+    echo ""
     echo "Entering $1"
     for i in "$1"/*; do
+        if [ -f "$i" ] && [[ "$i" =~ \.cpp$ ]]; then
+            INNAME="${i#$START_DIR/}"
+            OUTNAME=$(basename "$i")
+            OUTNAME="lib/${OUTNAME%.cpp}.o"
+            echo ""
+            echo "Building $INNAME..."
+            #echo "$INNAME"
+            #echo "$OUTNAME"
+            g++ -c "$INNAME" -std=c++17 -O3 -Wall -lphysfs -lpthread -Iinclude -DWTE_DEBUG_MODE=0 -o "$OUTNAME" `pkg-config --libs allegro-5 allegro_main-5 allegro_physfs-5 allegro_audio-5 allegro_acodec-5 allegro_font-5 allegro_image-5 allegro_primitives-5`
+        fi
         if [ -d "$i" ]; then
             build_items "$i"
         fi
-        if [ -f "$i" ]; then
-            # Add extra check for .cpp file
-            INNAME="${i#$START_DIR}"
-            INNAME="${INNAME#/}"
-            OUTNAME=$(basename "$i")
-            OUTNAME="lib/${OUTNAME%.cpp}.o"
-            echo "Building $INNAME..."
-            echo "$INNAME"
-            echo "$OUTNAME"
-            #g++ -c "$INNAME" -std=c++17 -O3 -Wall -lphysfs -lpthread -Iinclude -DWTE_DEBUG_MODE=0 -o "$OUTNAME" `pkg-config --libs allegro-5 allegro_main-5 allegro_physfs-5 allegro_audio-5 allegro_acodec-5 allegro_font-5 allegro_image-5 allegro_primitives-5`
-        fi
     done
 }
+
+echo "Building wtengine..."
 
 build_items "$START_DIR$SOURCE_DIR"
 

0dac9397ba1f118eb63e2d70c3edd6d40849ddc2
Matthew Evans <mevans+git@wtfsystems.net>
2021-02-17 21:54:25 -0500

    bed

--- build_engine.sh
+++ build_engine.sh
@@ -7,7 +7,7 @@ CURRENT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 for i in "$CURRENT_DIR"/src/*; do
     filename=$(basename "$i")
     if [ -f $i ]; then
-        g++ -c src/$filename -std=c++17 -lphysfs -lpthread -Iinclude -DWTE_DEBUG_MODE=0 -o lib/${filename%.*}.o `pkg-config --libs allegro-5 allegro_main-5 allegro_physfs-5 allegro_audio-5 allegro_acodec-5 allegro_font-5 allegro_image-5 allegro_primitives-5`
+        g++ -c src/$filename -std=c++17 -O3 -Wall -lphysfs -lpthread -Iinclude -DWTE_DEBUG_MODE=0 -o lib/${filename%.*}.o `pkg-config --libs allegro-5 allegro_main-5 allegro_physfs-5 allegro_audio-5 allegro_acodec-5 allegro_font-5 allegro_image-5 allegro_primitives-5`
     fi
 done
 

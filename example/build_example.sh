f68b617d9b14bab911e97d0038f433b227bd7a42
Matthew Evans <mevans+git@wtfsystems.net>
2021-02-18 19:36:06 -0500

    sync

--- example/build_example.sh
+++ example/build_example.sh
@@ -9,6 +9,11 @@ PROJECT_DIR="$(dirname "$START_DIR")"
 
 echo "Building wtengine demo..."
 
-g++ src/*.cpp -std=c++17 -O3 -Wall -I"$PROJECT_DIR"/include -L"$PROJECT_DIR"/build -static -lwtengine -Bdynamic -lphysfs -lpthread -o build/wte_demo `pkg-config --libs allegro-5 allegro_main-5 allegro_physfs-5 allegro_audio-5 allegro_acodec-5 allegro_font-5 allegro_image-5 allegro_primitives-5`
+#g++ -c src/wte_demo.cpp -std=c++17 -O3 -Wall -I"$PROJECT_DIR"/include -L"$PROJECT_DIR"/build -lphysfs -lpthread -lwtengine `pkg-config --libs allegro-5 allegro_main-5 allegro_physfs-5 allegro_audio-5 allegro_acodec-5 allegro_font-5 allegro_image-5 allegro_primitives-5`
+#g++ -c src/main.cpp -std=c++17 -O3 -Wall -I"$PROJECT_DIR"/include -L"$PROJECT_DIR"/build -lphysfs -lpthread -lwtengine `pkg-config --libs allegro-5 allegro_main-5 allegro_physfs-5 allegro_audio-5 allegro_acodec-5 allegro_font-5 allegro_image-5 allegro_primitives-5`
+
+#g++ main.o wte_demo.o -O3 -o build/wte_demo -L"$PROJECT_DIR"/build -lphysfs -lpthread -lwtengine `pkg-config --libs allegro-5 allegro_main-5 allegro_physfs-5 allegro_audio-5 allegro_acodec-5 allegro_font-5 allegro_image-5 allegro_primitives-5`
+
+g++ -o build/wte_demo -L"$PROJECT_DIR"/build main.o wte_demo.o
 
 echo "Done!"

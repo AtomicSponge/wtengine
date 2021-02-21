6233b68c8f6fcf9799f60388d1b44eac01fec1a0
Matthew Evans <mevans+git@wtfsystems.net>
2021-02-21 15:15:54 -0500

    sync

--- example/build_example.sh
+++ example/build_example.sh
@@ -12,8 +12,8 @@ echo "Building wtengine demo..."
 #g++ -c src/wte_demo.cpp -std=c++17 -O3 -Wall -I"$PROJECT_DIR"/include -L"$PROJECT_DIR"/build -lphysfs -lpthread -lwtengine `pkg-config --libs allegro-5 allegro_main-5 allegro_physfs-5 allegro_audio-5 allegro_acodec-5 allegro_font-5 allegro_image-5 allegro_primitives-5`
 #g++ -c src/main.cpp -std=c++17 -O3 -Wall -I"$PROJECT_DIR"/include -L"$PROJECT_DIR"/build -lphysfs -lpthread -lwtengine `pkg-config --libs allegro-5 allegro_main-5 allegro_physfs-5 allegro_audio-5 allegro_acodec-5 allegro_font-5 allegro_image-5 allegro_primitives-5`
 
-#g++ main.o wte_demo.o -O3 -o build/wte_demo -L"$PROJECT_DIR"/build -lphysfs -lpthread -lwtengine `pkg-config --libs allegro-5 allegro_main-5 allegro_physfs-5 allegro_audio-5 allegro_acodec-5 allegro_font-5 allegro_image-5 allegro_primitives-5`
+#g++ -o build/wte_demo main.o wte_demo.o -O3 -L"$PROJECT_DIR"/build -lphysfs -lpthread -lwtengine `pkg-config --libs allegro-5 allegro_main-5 allegro_physfs-5 allegro_audio-5 allegro_acodec-5 allegro_font-5 allegro_image-5 allegro_primitives-5`
 
-g++ -o build/wte_demo main.o wte_demo.o
+g++ -o build/wte_demo main.o wte_demo.o -Wl,-unresolved-symbols=ignore-in-shared-libs
 
 echo "Done!"

a88dfd479f594c7c7929d2527dfe7ee8f5d0497a
Matthew Evans <mevans+git@wtfsystems.net>
2021-03-23 17:49:21 -0400

    sync

--- src/cmp/ai.cpp
+++ src/cmp/ai.cpp
@@ -17,12 +17,13 @@ namespace wte
 namespace cmp
 {
 
-ai::ai(void func(
-    const entity_id&,
-    mgr::entity_manager&,
-    mgr::message_manager&,
-    const int64_t&)) :
-    enabled_ai(func),
+ai::ai(
+    void func(
+        const entity_id&,
+        mgr::entity_manager&,
+        mgr::message_manager&,
+        const int64_t&
+    )) : enabled_ai(func),
     disabled_ai([](
         const entity_id& e_id,
         mgr::entity_manager& world,
@@ -40,14 +41,15 @@ ai::ai(
         const entity_id&,
         mgr::entity_manager&,
         mgr::message_manager&,
-        const int64_t&)) :
-    enabled_ai(func_a), disabled_ai(func_b) {}
+        const int64_t&)
+    ) : enabled_ai(func_a), disabled_ai(func_b) {}
 
 void ai::run_enabled(
     const entity_id& eid,
     mgr::entity_manager& world,
     mgr::message_manager& messages,
-    const int64_t& engine_time) {
+    const int64_t& engine_time
+) {
     enabled_ai(eid, world, messages, engine_time);
 }
 
@@ -55,7 +57,8 @@ void ai::run_disabled(
     const entity_id& eid,
     mgr::entity_manager& world,
     mgr::message_manager& messages,
-    const int64_t& engine_time) {
+    const int64_t& engine_time
+) {
     disabled_ai(eid, world, messages, engine_time);
 }
 

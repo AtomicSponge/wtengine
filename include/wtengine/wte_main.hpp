1fabdfd566e2cb2e815552c7956c71bf8645bbf1
Matthew Evans <mevans+git@wtfsystems.net>
2021-06-27 23:40:02 -0400

    sync

--- include/wtengine/wte_main.hpp
+++ include/wtengine/wte_main.hpp
@@ -256,18 +256,16 @@ inline void wte_main::process_new_game(const std::string& game_data) {
     mgr::systems::finalize();
     if(mgr::systems::empty()) throw std::runtime_error("No systems have been loaded!");
 
+    //  Stop audio manager from playing sounds.
+    mgr::audio::music_stop();
+    mgr::audio::ambiance_stop();
+    mgr::audio::voice_stop();
+    mgr::audio::sample_unload("all");
+    mgr::audio::sample_clear_instances();
+    
     //  Clear world and load starting entities.
     mgr::entities::clear();
     new_game();
-
-    //  Stop audio manager from playing sounds.
-    //  Messages run in reverse order.
-    //  Messages from new_game() will run AFTER these.
-    mgr::messages::add_message(message("audio", "stop_music", ""));
-    mgr::messages::add_message(message("audio", "stop_ambiance", ""));
-    mgr::messages::add_message(message("audio", "stop_voice", ""));
-    mgr::messages::add_message(message("audio", "unload_sample", "all"));
-    mgr::messages::add_message(message("audio", "clear_instances", ""));
 
     //  Restart the timer at zero.
     al_stop_timer(main_timer);
@@ -289,18 +287,16 @@ inline void wte_main::process_end_game(void) {
     engine_time::set_time(al_get_timer_count(main_timer));
 
     //  Stop audio manager from playing sounds.
-    //  Messages run in reverse order.
-    //  Messages from end_game() will run BEFORE these.
-    mgr::messages::add_message(message("audio", "stop_music", ""));
-    mgr::messages::add_message(message("audio", "stop_ambiance", ""));
-    mgr::messages::add_message(message("audio", "stop_voice", ""));
-    mgr::messages::add_message(message("audio", "unload_sample", "all"));
-    mgr::messages::add_message(message("audio", "clear_instances", ""));
+    mgr::audio::music_stop();
+    mgr::audio::ambiance_stop();
+    mgr::audio::voice_stop();
+    mgr::audio::sample_unload("all");
+    mgr::audio::sample_clear_instances();
 
     //  Call end game process.
     end_game();
 
-    //  Clear world and mgr::systems::
+    //  Clear world and systems.
     mgr::entities::clear();
     mgr::systems::clear();
 

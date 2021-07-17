d35b15312e6fcf60c0a844b3374eb266dbde3a2c
Matthew Evans <mevans+git@wtfsystems.net>
2021-07-17 15:08:32 -0400

    sync

--- include/wtengine/mgr/variables.hpp
+++ include/wtengine/mgr/variables.hpp
@@ -211,6 +211,7 @@ class variables final : private manager<variables> {
                 std::is_same<bool, T>::value ||
                 std::is_same<char, T>::value ||
                 std::is_same<int, T>::value ||
+                std::is_same<std::size_t, T>::value ||
                 std::is_same<int16_t, T>::value ||
                 std::is_same<int32_t, T>::value ||
                 std::is_same<int64_t, T>::value ||

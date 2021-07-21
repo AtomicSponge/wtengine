05a5d87fad65e8eeeadfc34a43a055833c2cd5b7
Matthew Evans <mevans+git@wtfsystems.net>
2021-07-21 02:06:50 -0400

    wip

--- include/wtengine/_globals/wrappers.hpp
+++ include/wtengine/_globals/wrappers.hpp
@@ -133,6 +133,7 @@ class al_bitmap : public wte_asset {
          * \brief
          */
         inline const int get_height(void) const { return al_get_bitmap_height(_al_bitmap); };
+
     private:
         ALLEGRO_BITMAP* _al_bitmap;
         bool nopreserve;

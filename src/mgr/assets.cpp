33593226b098a4298b26bfb4aa42b47748ba15f4
Matthew Evans <mevans+git@wtfsystems.net>
2021-07-24 14:34:37 -0400

    sync

--- src/mgr/assets.cpp
+++ src/mgr/assets.cpp
@@ -20,10 +20,19 @@ template <> bool assets::manager<assets>::initialized = false;
 std::map<std::string, std::pair<std::shared_ptr<wte_asset>, bool>> assets::_assets = {};
 std::map<std::string, ALLEGRO_BITMAP*> assets::_bitmaps_backup = {};
 
+/*
+ *
+ */
 assets::assets() { _assets.clear(); }
 
+/*
+ *
+ */
 assets::~assets() { _assets.clear(); }
 
+/*
+ *
+ */
 const bool assets::unload(const std::string& label) {
     auto it = _assets.find(label);
     if(it != _assets.end() && it->second.second) {
@@ -33,6 +42,9 @@ const bool assets::unload(const std::string& label) {
     return false;
 }
 
+/*
+ *
+ */
 const bool assets::secret_unload(const std::string& label) {
     auto it = _assets.find(label);
     if(it != _assets.end()) {
@@ -42,6 +54,9 @@ const bool assets::secret_unload(const std::string& label) {
     return false;
 }
 
+/*
+ *
+ */
 void assets::backup_bitmaps(void) {
     _bitmaps_backup.clear();
     for (auto & it : _assets) {
@@ -57,6 +72,9 @@ void assets::backup_bitmaps(void) {
     }
 }
 
+/*
+ *
+ */
 void assets::reload_bitmaps(void) {
     for (auto & it : _bitmaps_backup) {
         //  Restore bitmap.

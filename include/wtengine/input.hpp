422feff99e3ab103d3f333ff61d0b1618da8ead1
Matthew Evans <mevans+git@wtfsystems.net>
2021-07-31 17:33:50 -0400

    wip

--- include/wtengine/input.hpp
+++ include/wtengine/input.hpp
@@ -18,12 +18,12 @@
 
 #include <allegro5/allegro.h>
 
-#include "wtengine/config.hpp"
 #include "wtengine/_globals/_defines.hpp"
 #include "wtengine/_globals/alert.hpp"
 #include "wtengine/_globals/engine_time.hpp"
 #include "wtengine/_globals/wte_exception.hpp"
 #include "wtengine/mgr/input_inf.hpp"
+#include "wtengine/config.hpp"
 
 namespace wte
 {

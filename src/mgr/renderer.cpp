c75ad4c54aaf0be8ac08e9df34c546ff5c60f4b5
Matthew Evans <mevans+git@wtfsystems.net>
2021-08-17 19:22:45 -0400

    wip

--- src/mgr/renderer.cpp
+++ src/mgr/renderer.cpp
@@ -190,27 +190,24 @@ void renderer::render(void) {
                     float destination_x = 0.0f, destination_y = 0.0f;
 
                     try {
+                        cmp::const_comp_ptr<cmp::location> temp_get = mgr::world::get_component<cmp::location>(it.first);
                         //  Check if the sprite should be rotated.
                         if(it.second->draw_rotated()) {
                             sprite_angle = it.second->get_direction();
                             center_x = (al_get_bitmap_width(temp_bitmap) / 2);
                             center_y = (al_get_bitmap_height(temp_bitmap) / 2);
 
-                            destination_x = mgr::world::get_component<cmp::location>(it.first)->get_x() +
+                            destination_x = temp_get->get_x() +
                                 (al_get_bitmap_width(temp_bitmap) * it.second->get_scale_factor_x() / 2) +
                                 (it.second->get_draw_offset_x() * it.second->get_scale_factor_x());
-                            destination_y = mgr::world::get_component<cmp::location>(it.first)->get_y() +
+                            destination_y = temp_get->get_y() +
                                 (al_get_bitmap_height(temp_bitmap) * it.second->get_scale_factor_y() / 2) +
                                 (it.second->get_draw_offset_y() * it.second->get_scale_factor_y());
                         } else {
-                                destination_x = mgr::world::get_component<cmp::location>(it.first)->get_x() +
-                                    it.second->get_draw_offset_x();
-                                destination_y = mgr::world::get_component<cmp::location>(it.first)->get_y() +
-                                    it.second->get_draw_offset_y();
+                                destination_x = temp_get->get_x() + it.second->get_draw_offset_x();
+                                destination_y = temp_get->get_y() + it.second->get_draw_offset_y();
                         }
-                    } catch(...) {
-                        //  Didn't have location components
-                    }
+                    } catch(...) { /* Didn't have location component */ }
 
                     //  Draw the sprite.
                     if(it.second->draw_tinted())

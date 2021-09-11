5dbedc3610d2479fe915605f315e2cbbc688ee39
Matthew Evans <mevans+git@wtfsystems.net>
2021-09-11 14:21:30 -0400

    sync

--- include/wtengine/mnu/apply.hpp
+++ include/wtengine/mnu/apply.hpp
@@ -34,51 +34,40 @@ class apply final : public item {
         ~apply() = default;  //!<  Default destructor.
 
         /*!
-         * \brief On left trigger.
-         * 
-         * Set the apply item to cancel.
+         * \brief Set the apply item to cancel.
          */
         void on_left(void) override;
 
         /*!
-         * \brief On right trigger.
-         * 
-         * Set the apply item to apply.
+         * \brief Set the apply item to apply.
          */
         void on_right(void) override;
 
         /*!
          * \brief On select trigger.
-         * 
-         * Return the status of the apply item.
-         * 
          * \return A menu message object that will either run the apply or cancel.
          */
         void on_select(void) override;
 
         /*!
          * \brief Return display text for the menu item when rendering.
-         * 
-         * This is the display status of the apply item.
-         * 
          * \return Vector of display text strings.
          */
         const std::vector<std::string> get_text(void) const override;
 
         /*!
-         * \brief Reset to default trigger.
-         * 
-         * Reset the apply item to the cancel state.
+         * \brief Reset the apply item to the canceled state.
          */
         void reset_to_default(void) override;
 
         /*!
-         * \brief Set to default trigger.
-         * 
-         * Set the apply item's default state to cancel.
+         * \brief Set the apply item's default state to canceled.
          */
         void set_default(void) override;
 
+        /*!
+         * \brief Reset the apply item to the canceled state.
+         */
         void apply_setting(void) override;
 
     private:

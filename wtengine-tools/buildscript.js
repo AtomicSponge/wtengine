14e171192089b435c049d34247c589159d8dfa92
Matthew Evans <mevans+git@wtfsystems.net>
2021-12-06 11:58:18 -0500

    sync

--- wtengine-tools/buildscript.js
+++ wtengine-tools/buildscript.js
@@ -1,1 +1,21 @@
-var fs = require('fs')
+/**
+ * 
+ * @author Matthew Evans
+ * @module wtfsystems/wtengine-tools
+ * @version 0.0.1
+ * @see README.md
+ * @copyright MIT see LICENSE.md
+ * 
+ */
+
+const fs = require('fs')
+
+const buildScriptFile = () => {}
+
+const readCSVData = () => {}
+
+/*
+ * Main script
+ */
+
+//...

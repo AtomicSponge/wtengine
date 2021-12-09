b84f421ef77a90ef31a13125ad59e9668f37a06a
Matthew Evans <mevans+git@wtfsystems.net>
2021-12-08 19:49:27 -0500

    sync

--- wtengine-tools/wte-mkscript.js
+++ wtengine-tools/wte-mkscript.js
@@ -13,44 +13,11 @@ const { Buffer } = require('buffer')
 const { parse } = require('csv/sync')
 const { showScriptInfo, confirmPrompt, scriptError } = require('./_common')
 
-showScriptInfo('wte-mkscript')
-
 //if(!confirmPrompt('test?')) scriptError('flagrant error')
 
-/**
- * Build a game script file.
- * @param {String} outFile 
- * @param {Object} gameData 
- */
-const buildScriptFile = (outFile, gameData) => {
-    let rowCounter = Number(0)        //  Row counter for error reporting
-    let dataBuffer = Buffer.alloc(0)  //  Buffer to store binary file
-    gameData.forEach(row => {
-        rowCounter++
-        if(row.length !== 6) scriptError(`Row ${rowCounter} incorrect length.`)
+showScriptInfo('wte-mkscript')
 
-        //  Write each message:  timer / sys / to / from / cmd / args
-        dataBuffer = Buffer.concat([dataBuffer, Buffer.concat([
-            Buffer.from(new Int32Array([row[0]])),
-            Buffer.from(
-                row[1] + '\x00' + row[2] + '\x00' + row[3] + '\x00' +
-                row[4] + '\x00' + row[5] + '\x00'
-            )
-        ])])
-    })
-
-    //  Write buffer
-    try {
-        fs.writeFileSync(outFile, dataBuffer)
-        process.stdout.write(`Wrote data file '${outFile}' - ${rowCounter} commands\n`)
-    } catch(error) { scriptError(error) }
-}
-
-/*
- * Main script
- */
 const args = process.argv.slice(2)
-
 if(args[0] === undefined) scriptError('Please specify an input file.')
 if(!fs.existsSync(args[0])) scriptError(`Input file '${args[0]}' does not exist.`)
 if(args[1] === undefined) scriptError('Please specify an output file.')
@@ -69,5 +36,27 @@ switch(args[0].split('.')[1].toLowerCase()) {
 if(gameData === undefined) scriptError('Error generating binary object gameData.')
 
 process.stdout.write(`Generating game data file '${args[1]}', one moment...\n`)
-buildScriptFile(args[1], gameData)
+
+let rowCounter = Number(0)        //  Row counter for error reporting
+let dataBuffer = Buffer.alloc(0)  //  Buffer to store binary file
+gameData.forEach(row => {
+    rowCounter++
+    if(row.length !== 6) scriptError(`Row ${rowCounter} incorrect length.`)
+
+    //  Write each message:  timer / sys / to / from / cmd / args
+    dataBuffer = Buffer.concat([dataBuffer, Buffer.concat([
+        Buffer.from(new Int32Array([row[0]])),
+        Buffer.from(
+            row[1] + '\x00' + row[2] + '\x00' + row[3] + '\x00' +
+            row[4] + '\x00' + row[5] + '\x00'
+        )
+    ])])
+})
+
+//  Write buffer
+try {
+    fs.writeFileSync(args[1], dataBuffer)
+    process.stdout.write(`Wrote data file '${args[1]}' - ${rowCounter} commands\n`)
+} catch(error) { scriptError(error) }
+
 process.stdout.write('Done!\n')

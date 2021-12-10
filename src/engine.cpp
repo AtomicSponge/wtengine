806c2d4824f7be7b56aa5394e72ef680dce6610a
Matthew Evans <mevans+git@wtfsystems.net>
2021-12-10 12:17:59 -0500

    wip

--- src/engine.cpp
+++ src/engine.cpp
@@ -266,28 +266,21 @@ void engine::do_game(void) {
             //  Get any spawner messages and pass to handler.
             mgr::spawner::process_messages(mgr::messages::get("spawner"));
         }
-        //  Run any untimed systems.
-        if(config::flags::game_started) mgr::systems::run_untimed();
+        mgr::systems::run_untimed();  //  Run any untimed systems.
         /* *** END GAME LOOP ******************************************************** */
-        //  Render the screen.
-        mgr::gfx::renderer::render();
+        mgr::gfx::renderer::render();  //  Render the screen.
+
+        mgr::messages::prune();  //  Delete unprocessed timed messages.
 
         //  Check if display looses focus.
-        if(event.type == ALLEGRO_EVENT_DISPLAY_SWITCH_OUT) {
-            out_of_focus();
-        }
+        if(event.type == ALLEGRO_EVENT_DISPLAY_SWITCH_OUT) out_of_focus();
         //  Check if display returns to focus.
-        if(event.type == ALLEGRO_EVENT_DISPLAY_SWITCH_IN) {
-            back_in_focus();
-        }
+        if(event.type == ALLEGRO_EVENT_DISPLAY_SWITCH_IN) back_in_focus();
         //  Force quit if the game window is closed.
         if(event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
             if(config::flags::game_started) process_end_game();
             config::_flags::is_running = false;
         }
-
-        //  Delete timed messages that were not processed.
-        mgr::messages::prune();
     }
     /* *** END ENGINE LOOP ******************************************************** */
 

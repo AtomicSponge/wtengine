f6b4eda76fb29a342d4f98d7c3037493268e61be
Matthew Evans <mevans+git@wtfsystems.net>
2021-12-15 10:58:55 -0500

    sync

---
+++ include/wtengine/handlers.hpp
@@ -0,0 +1,42 @@
+/*!
+ * WTEngine | File:  handlers.hpp
+ * 
+ * \author Matthew Evans
+ * \version 0.71
+ * \copyright See LICENSE.md for copyright information.
+ * \date 2019-2021
+ */
+
+#ifndef WTE_HANDLERS_HPP
+#define WTE_HANDLERS_HPP
+
+#include <allegro5/allegro.h>
+
+#include "wtengine/_globals/_defines.hpp"
+#include "wtengine/_globals/alert.hpp"
+#include "wtengine/_globals/engine_time.hpp"
+#include "wtengine/_globals/wte_exception.hpp"
+#include "wtengine/config.hpp"
+
+namespace wte {
+
+/*!
+ * \class handlers
+ * \brief Manage the input queue and process events.
+ */
+class handlers {
+    friend class engine;
+
+    public:
+        //
+
+    protected:
+        //
+
+    private:
+        //
+};
+
+}  //  end namespace wte
+
+#endif

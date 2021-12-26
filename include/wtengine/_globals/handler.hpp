75afb90c51d6c018b4edef4424004a0f171e0358
Matthew Evans <mevans+git@wtfsystems.net>
2021-12-26 12:44:45 -0500

    sync

--- include/wtengine/_globals/handler.hpp
+++ include/wtengine/_globals/handler.hpp
@@ -22,10 +22,22 @@ enum handler_scopes { GLOBAL_HANDLES, NONGAME_HANDLES, GAME_HANDLES };
 using key_handler = std::function<void(int, ALLEGRO_DISPLAY*)>;
 using mouse_axes_handler = std::function<void(int, int, int, int, int, int, int, float, ALLEGRO_DISPLAY*)>;
 using mouse_button_handler = std::function<void(int, int, int, int, unsigned int, float, ALLEGRO_DISPLAY*)>;
+using mouse_warped_handler = std::function<void(int, int, int, int, int, int, int, float, ALLEGRO_DISPLAY*)>;
+using mouse_enter_display_handler = std::function<void(int, int, int, int, ALLEGRO_DISPLAY*)>;
+using mouse_leave_display_handler = std::function<void(int, int, int, int, ALLEGRO_DISPLAY*)>;
 using joy_axis_handler = std::function<void(int, int, float, ALLEGRO_JOYSTICK*)>;
-using joy_button_handler = std::function<void(int, ALLEGRO_JOYSTICK*)>;  //!<  ...
+using joy_button_handler = std::function<void(int, ALLEGRO_JOYSTICK*)>;
+using touch_begin_handler = std::function<void(int, float, float, float, float, bool, ALLEGRO_DISPLAY*)>;
+using touch_end_handler = std::function<void(int, float, float, float, float, bool, ALLEGRO_DISPLAY*)>;
+using touch_move_handler = std::function<void(int, float, float, float, float, bool, ALLEGRO_DISPLAY*)>;
+using touch_cancel_handler = std::function<void(int, float, float, float, float, bool, ALLEGRO_DISPLAY*)>;
 
-using handler_types = std::variant<key_handler, mouse_axes_handler, mouse_button_handler, joy_axis_handler, joy_button_handler>;
+using handler_types = std::variant<
+    key_handler, mouse_axes_handler, mouse_button_handler,
+    mouse_warped_handler, mouse_enter_display_handler, mouse_leave_display_handler,
+    joy_axis_handler, joy_button_handler,
+    touch_begin_handler, touch_end_handler, touch_move_handler, touch_cancel_handler
+>;
 
 /*!
  * \class handler

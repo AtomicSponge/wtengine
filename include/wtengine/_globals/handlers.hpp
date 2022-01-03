c15d1d9bd1864047445df10399d3221accc6f7df
Matthew Evans <mevans+git@wtfsystems.net>
2022-01-03 18:53:41 -0500

    wip

--- include/wtengine/_globals/handlers.hpp
+++ include/wtengine/_globals/handlers.hpp
@@ -78,12 +78,8 @@ class handlers {
     friend class input;
 
     public:
-        inline handlers() {
-            static_assert(S == GLOBAL_HANDLES || S == NONGAME_HANDLES || S == GAME_HANDLES,
-                "Scope must be one of the following: GLOBAL_HANDLES, NONGAME_HANDLES, GAME_HANDLES");
-        };
-
-        ~handlers() = default;                     //!<  Default destructor.
+        handlers() = delete;                       //!<  Delete constructor.
+        ~handlers() = delete;                      //!<  Delete destructor.
         handlers(const handlers&) = delete;        //!<  Delete copy constructor.
         void operator=(handlers const&) = delete;  //!<  Delete assignment operator.
 
@@ -93,7 +89,9 @@ class handlers {
          * \param handle Input handler.
          */
         template <size_t IDX>
-        inline constexpr void add(const handler_types& handle) {
+        inline constexpr static void add(const handler_types& handle) {
+            static_assert(S == GLOBAL_HANDLES || S == NONGAME_HANDLES || S == GAME_HANDLES,
+                "Scope must be one of the following: GLOBAL_HANDLES, NONGAME_HANDLES, GAME_HANDLES");
             static_assert(IDX < 15, "Invalid Handler Event Index");
             _temp_handlers[IDX] = handle;
         };

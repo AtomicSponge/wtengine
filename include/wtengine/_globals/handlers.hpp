788022d18964cf9fb59818ebb33d7b5d98dcb35a
Matthew Evans <mevans+git@wtfsystems.net>
2022-01-22 21:42:46 -0500

    wip

--- include/wtengine/_globals/handlers.hpp
+++ include/wtengine/_globals/handlers.hpp
@@ -127,25 +127,25 @@ class handlers<S, IDX, WTE_HANDLER_SET> : _register<WTE_HANDLER_SET> {
             static_assert(S == WTE_GLOBAL_HANDLES || S == WTE_NONGAME_HANDLES || S == WTE_GAME_HANDLES,
                 "Scope must be one of the following: WTE_GLOBAL_HANDLES, WTE_NONGAME_HANDLES, WTE_GAME_HANDLES");
             static_assert(IDX < WTE_EVENT_MAX, "Invalid Handler Event Index");
-            if constexpr (std::is_same<T, handler::key>::value)
+            if constexpr (std::is_same_v<T, handler::key>)
                 static_assert(IDX == WTE_EVENT_KEY_DOWN || IDX == WTE_EVENT_KEY_UP,
                     "Event Index must be a Key Up or Down Event");
-            else if constexpr (std::is_same<T, handler::mouse_axis>::value)
+            else if constexpr (std::is_same_v<T, handler::mouse_axis>)
                 static_assert(IDX == WTE_EVENT_MOUSE_AXIS || IDX == WTE_EVENT_MOUSE_WARPED,
                     "Event Index must be a Mouse Axes or Warped Event");
-            else if constexpr (std::is_same<T, handler::mouse_button>::value)
+            else if constexpr (std::is_same_v<T, handler::mouse_button>)
                 static_assert(IDX == WTE_EVENT_MOUSE_BUTTON_DOWN || IDX == WTE_EVENT_MOUSE_BUTTON_UP,
                     "Event Index must be a Mouse Button Up or Down Event");
-            else if constexpr (std::is_same<T, handler::mouse_display>::value)
+            else if constexpr (std::is_same_v<T, handler::mouse_display>)
                 static_assert(IDX == WTE_EVENT_MOUSE_ENTER_DISPLAY || IDX == WTE_EVENT_MOUSE_LEAVE_DISPLAY,
                     "Event Index must be a Mouse Enter or Leave Display Event");
-            else if constexpr (std::is_same<T, handler::joystick_axis>::value)
+            else if constexpr (std::is_same_v<T, handler::joystick_axis>)
                 static_assert(IDX == WTE_EVENT_JOYSTICK_AXIS,
                     "Event Index must be a Joystick Axes Event");
-            else if constexpr (std::is_same<T, handler::joystick_button>::value)
+            else if constexpr (std::is_same_v<T, handler::joystick_button>)
                 static_assert(IDX == WTE_EVENT_JOYSTICK_BUTTON_DOWN || IDX == WTE_EVENT_JOYSTICK_BUTTON_UP,
                     "Event Index must be a Joystick Button Up or Down Event");
-            else if constexpr (std::is_same<T, handler::touch>::value)
+            else if constexpr (std::is_same_v<T, handler::touch>)
                 static_assert(IDX == WTE_EVENT_TOUCH_BEGIN || IDX == WTE_EVENT_TOUCH_END ||
                     IDX == WTE_EVENT_TOUCH_MOVE || IDX == WTE_EVENT_TOUCH_CANCEL,
                     "Event Index must be a Touch Event");

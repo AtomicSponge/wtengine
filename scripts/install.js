348004584911b70ea4e586907ed7e24ba018285b
Matthew Evans <mevans+git@wtfsystems.net>
2022-04-18 13:52:54 -0400

    wip

--- scripts/install.js
+++ scripts/install.js
@@ -5,3 +5,14 @@
  * @see README.md
  * @copyright MIT see LICENSE.md
  */
+ 
+const config = require('./_config')
+
+const wtf = require('./_common')
+const fs = require('fs')
+const path = require('path')
+const shell = require('shelljs')
+
+//  Set path to node for shelljs
+const nodePath = (shell.which('node').toString())
+shell.config.execPath = nodePath

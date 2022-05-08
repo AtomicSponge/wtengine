b1a479d65e6ffdffc001e8458eee571b6c12524a
Matthew Evans <mevans+git@wtfsystems.net>
2022-05-08 19:14:53 -0400

    sync

--- scripts/wte-config.js
+++ scripts/wte-config.js
@@ -15,4 +15,11 @@ const inquirer = require('inquirer')
 
 process.stdout.write(`${wtf.constants.CYAN}WTEngine Configuration Utility${wtf.constants.CLEAR}\n\n`)
 
+const settings = wtf.loadSettings()
+if(!settings) {
+    process.stdout.write(`No settings file found, running setup...\n`)
+} else {
+    process.stdout.write(`settings exist... do other stuff\n`)
+}
+
 process.stdout.write(`${wtf.constants.DIM}${wtf.constants.CYAN}Config Done!${wtf.constants.CLEAR}\n\n`)

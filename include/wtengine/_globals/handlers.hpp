e286cf3019d29d56cd9932ee624f20083fc0c620
Matthew Evans <mevans+git@wtfsystems.net>
2022-10-05 19:22:12 -0400

    wip

--- include/wtengine/_globals/handlers.hpp
+++ include/wtengine/_globals/handlers.hpp
@@ -62,7 +62,6 @@ namespace wte::handler {
 template <handler_scopes S, handler_events IDX>
 struct key {
     inline static auto _handle = ([](const int&, ALLEGRO_DISPLAY*){});
-    using type = key;
 };
 
 template <handler_scopes S, handler_events IDX>
@@ -111,8 +110,8 @@ namespace wte {
  * \tparam T Handler type.
  * \param handle Input handler function expression.
  */
-template <handler_scopes S, handler_events IDX, class T>
-constexpr void add_handler(T handle) {
+template <handler_scopes S, handler_events IDX, class T, typename F>
+constexpr void add_handler(F&& handle) {
     static_assert(S == GLOBAL_HANDLES || S == NONGAME_HANDLES || S == GAME_HANDLES,
         "Scope must be one of the following: GLOBAL_HANDLES, NONGAME_HANDLES, GAME_HANDLES");
     static_assert(IDX < HANDLER_EVENT_MAX, "Invalid Handler Event Index");

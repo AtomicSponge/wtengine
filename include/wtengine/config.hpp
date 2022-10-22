0ca60c3e9367f597e3c25bad2a71b1e78f18daa3
Matthew Evans <mevans+git@wtfsystems.net>
2022-10-22 00:48:17 -0400

    sync

--- include/wtengine/config.hpp
+++ include/wtengine/config.hpp
@@ -48,9 +48,9 @@ class config {
          * \brief Engine flags.
          */
         struct flags {
-            static const bool& is_running;            //!<  Flag to check if the game is running.
+            static const bool& is_running;            //!<  Flag for if the program itself is running.
+            static const bool& engine_started;        //!<  Flag to check if the engine is running.
             static const bool& engine_paused;         //!<  Flag to check if the engine if paused.
-            static const bool& engine_started;        //!<  Flag to check if the engine has started.
             static const bool& record_input;          //!<  Flag to enable/disable input recording.
             static const bool& keyboard_detected;     //!<  Flag to check if a keyboard is detected.
             static const bool& mouse_detected;        //!<  Flag to check if a mouse is detected.
@@ -201,8 +201,8 @@ class config {
     private:
         struct _flags {
             inline static bool is_running = true;
-            inline static bool engine_paused = false;
             inline static bool engine_started = false;
+            inline static bool engine_paused = false;
             inline static bool record_input = false;
             inline static bool keyboard_detected = false;
             inline static bool mouse_detected = false;

514d25bb8eacbc47b427eaf6655b29c8b19737e4
Matthew Evans <mevans+git@wtfsystems.net>
2022-10-22 10:03:04 -0400

    sync

--- src/mgr/renderer.cpp
+++ src/mgr/renderer.cpp
@@ -97,6 +97,52 @@ void renderer::set_font(wte_asset<al_font> font) { renderer_font = font; }
 /*
  *
  */
+void renderer::draw_hitboxes(void) {
+    if constexpr (build_options.debug_mode) {
+        const const_component_container<cmp::hitbox> hitbox_components =
+            mgr::world::get_components<cmp::hitbox>();
+
+        for(auto& it: hitbox_components) {
+            if(it.second->solid) {
+                //  Select color based on team.
+                ALLEGRO_COLOR team_color;
+                switch(it.second->team) {
+                    case 0: team_color = WTE_COLOR_GREEN; break;
+                    case 1: team_color = WTE_COLOR_RED; break;
+                    case 2: team_color = WTE_COLOR_BLUE; break;
+                    default: team_color = WTE_COLOR_YELLOW;
+                }
+                //  Draw the hitbox.
+                ALLEGRO_BITMAP* temp_bitmap = al_create_bitmap(
+                    it.second->width,
+                    it.second->height);
+                al_set_target_bitmap(temp_bitmap);
+                al_clear_to_color(team_color);
+                al_set_target_bitmap(**arena_bitmap);
+                try {
+                    al_draw_bitmap(temp_bitmap,
+                        mgr::world::get_component<cmp::location>(it.first)->pos_x,
+                        mgr::world::get_component<cmp::location>(it.first)->pos_y, 0);
+                } catch(const std::exception& e) { throw e; }
+                al_destroy_bitmap(temp_bitmap);
+            }
+        }
+    }
+}
+
+/*
+ *
+ */
+void renderer::draw_timer(void) {
+    if constexpr (build_options.debug_mode) {
+        const std::string timer_string = "Timer: " + std::to_string(engine_time::check());
+        al_draw_text(**renderer_font, WTE_COLOR_YELLOW, config::gfx::screen_w, 10, ALLEGRO_ALIGN_RIGHT, timer_string.c_str());
+    }
+}
+
+/*
+ *
+ */
 void renderer::render(void) {
     /*
      * Calculate fps.

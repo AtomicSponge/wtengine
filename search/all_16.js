06600ff4ad19d0537beeccc8e5fbb2394f1c61a5
AtomicSponge <mevans+git@wtfsystems.net>
2024-11-01 14:26:33 +0000

    Deploying to gh-pages from @ AtomicSponge/wtengine@ce00be4bf6f42fb38609df82ef55e1d686d283a9 🚀

--- search/all_16.js
+++ search/all_16.js
@@ -3,8 +3,7 @@ var searchData=
   ['what_0',['what',['../classwte_1_1engine__error.html#a5804bc5cc340b8324d261e2897368a11',1,'wte::engine_error::what()'],['../classwte_1_1engine__exception.html#a59cc10a716e2676b4f4a98b3be55ffb6',1,'wte::engine_exception::what()']]],
   ['when_1',['when',['../classwte_1_1engine__error.html#ac06a510dfbf38a4a509b6b5db3e40044',1,'wte::engine_error::when()'],['../classwte_1_1engine__exception.html#aee1cec0e6184b58e5c551489acd2508e',1,'wte::engine_exception::when()']]],
   ['where_2',['where',['../classwte_1_1engine__error.html#abb561a13612bc87b319d0be57fe4e307',1,'wte::engine_error::where()'],['../classwte_1_1engine__exception.html#ad2645cea100b0604ac590fa6545d1127',1,'wte::engine_exception::where()']]],
-  ['width_3',['width',['../structwte_1_1WTE__DISPLAY__MODE.html#ac28d02a3f6790b9ea150502af4277e88',1,'wte::WTE_DISPLAY_MODE::width()'],['../classwte_1_1cmp_1_1hitbox.html#af23b8b5f3393b47f659aeebc2b2960ff',1,'wte::cmp::hitbox::width()']]],
+  ['width_3',['width',['../classwte_1_1cmp_1_1hitbox.html#af23b8b5f3393b47f659aeebc2b2960ff',1,'wte::cmp::hitbox']]],
   ['world_4',['world',['../classwte_1_1mgr_1_1world.html',1,'wte::mgr']]],
-  ['wte_5fbuild_5foptions_5',['wte_build_options',['../structwte_1_1wte__build__options.html',1,'wte']]],
-  ['wte_5fdisplay_5fmode_6',['WTE_DISPLAY_MODE',['../structwte_1_1WTE__DISPLAY__MODE.html',1,'wte']]]
+  ['wte_5fbuild_5foptions_5',['wte_build_options',['../structwte_1_1wte__build__options.html',1,'wte']]]
 ];
--- search/variables_a.js
+++ search/variables_a.js
@@ -1,7 +1,6 @@
 var searchData=
 [
-  ['label_0',['label',['../structwte_1_1WTE__DISPLAY__MODE.html#a8885fd48a1b23bd2e91811676f3df04c',1,'wte::WTE_DISPLAY_MODE']]],
-  ['last_5frender_1',['last_render',['../classwte_1_1mgr_1_1gfx_1_1renderer.html#ad43e54487434fe73646174cf73c20dbf',1,'wte::mgr::gfx::renderer']]],
-  ['layer_2',['layer',['../classwte_1_1cmp_1_1gfx_1_1gfx.html#a75523649b69ed356e5800128693b7a6f',1,'wte::cmp::gfx::gfx']]],
-  ['load_5fsystems_3',['load_systems',['../classwte_1_1engine.html#a11451b1938b14ad9b3a6e256c54ad0d6',1,'wte::engine']]]
+  ['last_5frender_0',['last_render',['../classwte_1_1mgr_1_1gfx_1_1renderer.html#ad43e54487434fe73646174cf73c20dbf',1,'wte::mgr::gfx::renderer']]],
+  ['layer_1',['layer',['../classwte_1_1cmp_1_1gfx_1_1gfx.html#a75523649b69ed356e5800128693b7a6f',1,'wte::cmp::gfx::gfx']]],
+  ['load_5fsystems_2',['load_systems',['../classwte_1_1engine.html#a11451b1938b14ad9b3a6e256c54ad0d6',1,'wte::engine']]]
 ];
